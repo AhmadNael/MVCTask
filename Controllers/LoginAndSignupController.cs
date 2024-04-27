c4b05d1df3702bd4163b89e592ad76332b5d23b2
AhmadNael <ahmad.naelalnaser@gmail.com>
2024-04-27 14:41:28 +0300

    Add project files.

---
+++ Controllers/LoginAndSignupController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCTask.Controllers
+{
+    public class LoginAndSignupController : Controller
+    {
+       
+        public IActionResult Signup()
+        {
+            return View();
+        }
+    }
+}
